ca0cc0186e23754f4e8b8590f1b9a8e760d51f31
Einnyboi <cathrinemoon24@gmail.com>
2026-06-04 04:23:59 +0700

    fix railway error pnpm caused

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const frontendOrigins = [
