4f112e4d1075ff8516dba77adfeb187d547ddaff
Einnyboi <cathrinemoon24@gmail.com>
2026-06-17 02:35:12 +0700

    fix presensi

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -28,7 +28,7 @@ export class TicketsController {
 
   @Get()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
-  @Roles(Role.ADMIN, Role.DOSEN)
+  @Roles(Role.ADMIN, Role.DOSEN, Role.ASDOS)
   findAll() {
     return this.ticketsService.findAll();
   }
@@ -49,7 +49,7 @@ export class TicketsController {
 
   @Patch(':id')
   @UseGuards(AuthGuard('jwt'), RolesGuard)
-  @Roles(Role.ADMIN, Role.DOSEN)
+  @Roles(Role.ADMIN, Role.DOSEN, Role.ASDOS)
   update(@Param('id') id: string, @Body() ticket: CorrectionTicketDto) {
     return this.ticketsService.update(id, ticket);
   }
